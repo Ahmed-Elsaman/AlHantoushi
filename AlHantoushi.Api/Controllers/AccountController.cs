7b3a7225f6df00315bfe411688dde60f37ded66f
Muhammad Abdulltif <pingcairodevelopers@gmail.com>
2024-11-04 15:41:54 +0200

    update identity

---
+++ AlHantoushi.Api/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using AlHantoushi.Api.Dtos;
+using AlHantoushi.Api.Extensions;
+using AlHantoushi.Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AlHantoushi.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(SignInManager<AppUser> signInManager) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<ActionResult> Register(RegisterDto registerDto)
+    {
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            UserName = registerDto.Email
+        };
+
+        var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem();
+        }
+
+        return Ok();
+    }
+
+    [Authorize]
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+
+        return NoContent();
+    }
+
+    [HttpGet("auth-status")]
+    public ActionResult GetAuthState()
+    {
+        return Ok(new { IsAuthenticated = User.Identity?.IsAuthenticated ?? false });
+    }
+
+    [Authorize]
+    [HttpPost("reset-password")]
+    public async Task<ActionResult> ResetPassword(string currentPassword, string newPassword)
+    {
+        var user = await signInManager.UserManager.GetUserByEmail(User);
+
+        var result = await signInManager.UserManager.ChangePasswordAsync(user, currentPassword, newPassword);
+
+        if (result.Succeeded)
+        {
+            return Ok("Password updated");
+        }
+
+        return BadRequest("Failed to update password");
+    }
+}
